99a16613ca17e3f0ced19464af77af669ca6ce3e
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-06 16:44:58 -0400

    removed the laravel-setup/ directory

--- projects/laravel-setup/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-$log->addWarning('new warning');
-
-?>
