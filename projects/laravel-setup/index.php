a4d5fc6719f26ba6bd75369edcbb3798b6fad57f
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-06 16:38:47 -0400

    added a basic package usage example with Composer

---
+++ projects/laravel-setup/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('new warning');
+
+?>
